73fd76dd4f9efcdd094d0d4e0633bdbbf62ad855
Matheus Nunes <matheusyagami147@gmail.com>
2022-04-08 22:47:47 -0300

    feat: add is_admin field to user migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
         // \App\Models\Grupo::factory(5)->hasPostagems(8)->create();
         // \App\Models\Usuario::factory(4)->hasPostagems(8)->create();
     }
